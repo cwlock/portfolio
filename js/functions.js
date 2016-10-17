5f35be8bdce530a99a3211a091964cc9ecdc7bb6
Chris Lockwood <cwlock@users.noreply.github.com>
2016-10-16 22:55:12 -0700

    Added Microsoft backgrond image, company logo, modified styles, added general function file, added new page

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    if(window.location.href.indexOf("microsoft.html") > -1) {
+		document.getElementById("MicrosoftHeader").style.backgroundImage="url(../img/bgMicrosoft.png)";
+    }
+});
