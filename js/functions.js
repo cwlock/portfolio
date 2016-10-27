55fe5de0c53c8a3792ec33d9b7294ac97b1dbc73
Chris Lockwood <cwlock@users.noreply.github.com>
2016-10-26 23:13:48 -0700

    Added more content to html and updates some section styles. Added new images.

--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,22 @@ $(document).ready(function () {
 		document.getElementById("MicrosoftHeader").style.backgroundImage="url(../img/bgMicrosoft.png)";
     }
 });
+
+$('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
+
+var amountScrolled = 700;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('fast');
+	} else {
+		$('a.back-to-top').fadeOut('fast');
+	}
+});
+
+$('a.back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: $("#"+'microsoftContents').offset().top
+	}, 'fast');
+	return false;
+});
